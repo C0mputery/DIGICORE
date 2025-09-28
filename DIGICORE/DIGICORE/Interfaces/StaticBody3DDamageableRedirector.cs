f196fcb5e0bdd9d06db97d239bfa8e63ef4218f0
C0mputery <77170025+C0mputery@users.noreply.github.com>
2025-09-28 14:01:20 -0400

    Coin Stuff More almost done

--- DIGICORE/DIGICORE/Interfaces/StaticBody3DDamageableRedirector.cs
+++ DIGICORE/DIGICORE/Interfaces/StaticBody3DDamageableRedirector.cs
@@ -5,11 +5,12 @@ namespace Digicore.Interfaces;
 /// <summary>
 /// This is a thing because I hate attaching scripts to different node types than is defined by the class.
 /// </summary>
+[GlobalClass]
 public partial class StaticBody3DDamageableRedirector : StaticBody3D, IDamageable {
-    [Export] public Node TargetPath;
+    [Export] public Node Target;
     private IDamageable _target;
 
-    public override void _Ready() { _target = (IDamageable)TargetPath; }
+    public override void _Ready() { _target = (IDamageable)Target; }
     
     public void TakeDamage(float damage, Vector3 force) {
         _target?.TakeDamage(damage, force);
